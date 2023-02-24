41e6aaa227d9d3a2a8dc5bd645c5473ca1b2502c
FedorsHome1 <nik.goltsev.97@bk.ru>
2023-02-24 09:18:57 +0700

    Start_of_project

---
+++ main.py
@@ -0,0 +1,17 @@
+import numpy as np
+import pandas as pd
+
+
+class Main_class():
+
+    def __init__(self) ->  None:
+
+        return None
+
+
+class Run_function():
+
+    def __inti__(self) -> None:
+
+        return None
+
