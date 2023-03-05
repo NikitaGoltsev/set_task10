120409897b5e9294eb100b80645dd7e6abb79037
FedorsHome1 <nik.goltsev.97@bk.ru>
2023-03-05 18:13:21 +0700

    Full_first_zd

--- zd01.py
+++ zd01.py
@@ -1,101 +1,179 @@
 import numpy as np
 
-class Softmax:
-    def forward(self, x):
-        x = x - np.max(x)
-        self.p = np.exp(x) / np.sum(np.exp(x))
-        return self.p
+class Lost_Func():
 
-    def backward(self, dz):
-        jacobian = np.diag(dz)
-        for i in range(len(jacobian)):
-            for j in range(len(jacobian)):
-                if i == j:
-                    jacobian[i][j] = self.p[i] * (1 - self.p[j])
-                else:
-                    jacobian[i][j] = -self.p[i] * self.p[j]
-        return np.matmul(dz, jacobian)
+    f = np.vectorize(lambda x,y: np.exp(x)/y)
 
+    def ReLU(self, x):
 
-class ReLu:
-    def forward(self, x):
-        self.x = x
         return np.maximum(0, x)
 
-    def backward(self, dz):
-        dz[self.x < 0] = 0
-        return dz
+    def tanh(self, x):
 
-class Dense:
-    def __init__(self, in_size, out_size, reg_lambda=0.0):
-        self.W = np.random.normal(
-            scale=1, size=(out_size, in_size)) * np.sqrt(2 / (in_size + out_size))
-        self.b = np.zeros(out_size)
-        self.reg_lambda = reg_lambda
-        self.final_dW = 0
-        self.final_db = 0
+        return np.tanh(x)
 
-    def forward(self, x):
-        self.x = x
-        if np.array(x).shape[0] != self.W.shape[1]:
-            print('X is not the same dimention as in_size')
-        return (np.dot(self.W, self.x) + self.b)
+    def softMax(self, x):
+        exponentsSum = np.sum(np.exp(x))
+        return self.f(x,exponentsSum)
 
-    # честно верим, что эта функция работает правильно
-    def get_reg_loss(self):
-        return 0.5 * self.reg_lambda * (np.linalg.norm(self.W, ord='fro')**2)
+class dop_functions():
 
-    def backward(self,
-                 dz,
-                 learning_rate=0.001,
-                 mini_batch=False,
-                 update=True,
-                 len_mini_batch=None):
-        self.dW = np.outer(dz, self.x)
-        self.db = dz
-        self.dx = np.dot(dz, self.W)
+    def maxPool(self, x, kernelSize, step):
+        kernelSize = int(kernelSize)
+        inX, inY, inC = x.shape
+        maxPoolResult = np.zeros((inX//kernelSize, inY//kernelSize, inC))
 
-        if (self.reg_lambda != 0):
-            self.dW += self.reg_lambda * self.W
+        for channel in range(inC):
+            for v_shift in range(0, inX - kernelSize + 1, step):
+                for h_shift in range(0, inY - kernelSize + 1, step):
+                    maxPoolResult[v_shift//kernelSize][h_shift//kernelSize][channel] = np.max(x[v_shift:v_shift+kernelSize,h_shift:h_shift+kernelSize,channel])
+        return maxPoolResult
 
-        if mini_batch == True:
-            self.final_dW += self.dW
-            self.final_db += self.db
+    def pad(self, matr, pad1, pad2=None):
+        if pad2 == None:
+            pad2 = pad1
+        x, y, c = matr.shape
+        resMatr = np.zeros((x+pad1+pad2, y+pad1+pad2, c))
+        resMatr[pad1:-pad2, pad1:-pad2] = matr
+        return resMatr
 
-        if update == True:
-            if mini_batch == True:
-                self.W = self.W - learning_rate * self.final_dW / len_mini_batch
-                self.b = self.b - learning_rate * self.final_db / len_mini_batch
-                self.final_dW = 0
-                self.final_db = 0
-            else:
-                self.W = self.W - learning_rate * self.dW
-                self.b = self.b - learning_rate * self.db
+    def convolution(self, x, kernel, step):
+        if x.shape[2] != kernel.shape[2]:
+            raise ValueError("Shapes must be same")
+        
+        inX, inY, inC = x.shape
+        kX, kY, kC = kernel.shape
 
-        return self.dx
+        convResult = np.zeros((inX-1, inY-1, x.shape[2]))
+        for channel in range(inC):
+            for v_shift in range(0, inX - kX + 1, step):
+                for h_shift in range(0, inY - kY + 1, step):
+                    convResult[v_shift][h_shift][channel] = np.sum(
+                        x[v_shift:v_shift+kX,h_shift:h_shift+kY,channel] * kernel[:,:,channel]
+                    )
+        convResult = np.sum(convResult, axis=2)
+        return (convResult)
 
 
-class Dropout:
-    def __init__(self, p=0.5):
-        self.p = p
+class Layer():
 
-    def forward(self, x, train=True):
-        if not train:
-            self.mask = np.ones(*x.shape)
-            return x
-        self.mask = (np.random.rand(*x.shape) > self.p) / (1.0 - self.p)
-        return x * self.mask
+    def __init__(self, size, func_of_l = []):
 
-    def backward(self, dz, lr=0.001):
-        return dz * self.mask
+        self.weights = np.random.random(size)
+        self.lossFuncs = func_of_l
 
-class CrossEntropy:
-    def forward(self, y_true, y_hat):
-        self.y_hat = y_hat
-        self.y_true = y_true
-        self.loss = -np.sum(self.y_true * np.log(y_hat))
-        return self.loss
+        return None
 
-    def backward(self):
-        dz = -self.y_true / self.y_hat
-        return dz
+    def __call__(self, inputs):
+
+        return self.forward(inputs)
+    
+    def forward(self, inputs):
+
+        result = inputs.dot(self.weights)
+        for f in self.lossFuncs:
+            result = f(result)
+
+        return result
+
+class FoldLayer():
+
+    loss_f = Lost_Func()
+    dop = dop_functions()
+
+    def __init__(self, sizeIn, sizeOut, kernelSize, pad, step):
+        self.pad = pad
+        self.step = step
+        self.sIx, self.sIy, self.sIc = sizeIn
+        self.sOx, self.sOy, self.sOc = sizeOut
+        self.activatation = self.loss_f.ReLU()
+
+        self.weights = []
+        for _ in range(sizeOut[2]):
+            self.weights.append(np.random.random((kernelSize, kernelSize, self.sIc)))
+        self.weights = np.array(self.weights)
+
+
+    def __call__(self, inputs):
+        return self.forward(inputs)
+
+    def forward(self, inputs):
+        ny, nx, nc = inputs.shape
+    
+        result = np.zeros((ny-1, nx-1, self.sOc)) 
+
+        for ind, weight in enumerate(self.weights):
+            applied = self.dop.convolution(inputs, weight, step=self.step)
+            result[:, :, ind] = applied[:, :]
+        
+        return self.activatation(result)
+
+class MaxPoolLayer():
+
+    dop = dop_functions()
+
+    def __init__(self, kernelSize, step):
+        self.step = step
+        self.kernelSize = kernelSize
+    
+    def __call__(self, inputs):
+        return self.forward(inputs)
+
+    def forward(self, inputs):
+        result = self.dop.maxPool(inputs, self.kernelSize, step=self.step)
+        return result
+    
+
+# To load code from first zd
+class First_zd():
+
+    loss_f = Lost_Func()
+
+    def __init__(self) -> None:
+
+        
+        #np.random.seed(100)
+
+        global Layer
+
+        inputs = np.random.randint(0, 255, size=(1, 256))
+        layer_1 = Layer((256, 64), [self.loss_f.ReLU])
+        layer_2 = Layer((64, 16), [self.loss_f.ReLU, self.loss_f.tanh])
+        layer_3 = Layer((16, 4), [self.loss_f.ReLU, self.loss_f.softMax])
+
+        conveyorFirstTask = [layer_1, layer_2, layer_3]
+
+        result = inputs
+        for Layer in conveyorFirstTask:
+            result = Layer(result)
+        
+        print(result)
+        return None
+
+# Second zd
+
+class Second_zd():
+
+    def __init__(self) -> None:
+
+        inputs = np.random.randint(0, 255, size=(19,19,3))
+
+        x1 = FoldLayer((19,19,3), (18,18,8), kernelSize=3, pad=0, step=1)
+        x2 = MaxPoolLayer(2, 2)
+        x3 = FoldLayer((9,9,8), (8,8,16), 3, 0, 1)
+        x4 = MaxPoolLayer(2, 2)
+
+
+        conveyorSecondTask = [x1, x2, x3, x4]
+        data = inputs
+        print(data.shape)
+        for layer in conveyorSecondTask:
+            data = layer(data)
+            print(data.shape)
+        
+
+        return None
+
+fst = First_zd()
+
+#snd = Second_zd()
